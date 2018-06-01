62f7d5572768cb5bf9d59909ddb3fbb53def54e4
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-06-01 14:48:46 -0400

    example 2-7 complete

--- chat.js
+++ chat.js
@@ -1,13 +1,35 @@
 var net = require('net');
 
 var chatServer = net.createServer();
+var clientList = [];
 
-chatServer.on('connection', function(client) {
+chatServer.on('connection', (client) => {
+  var ip = client.remoteAddress;
+  var port = client.remotePort;
+  
+  if (ip.substr(0,7) == "::ffff:") {
+    ip = ip.substr(7);
+  };
+
+  client.name = ip + ':' + port;
+ 
+  console.log(client.name);
+ 
   client.write('Hi!\n');
 
-  client.on('data', (data) => {
-    console.log(data);    
+  clientList.push(client);
+
+   client.on('data', (data) => {
+      broadcast(data, client);
   });
-})
+});
+
+function broadcast(message, client) {
+  for (let i = 0; i < clientList.length; i++) {
+    if (client !== clientList[i]) {
+      clientList[i].write(message)
+    };
+  };
+};
 
 chatServer.listen(9000);
