3986d08e8e30b422c71183f8a9dd074b75a61c2e
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-06-01 20:20:55 -0400

    Exemple 2-12 complete

--- chat.js
+++ chat.js
@@ -5,31 +5,52 @@ var clientList = [];
 
 chatServer.on('connection', (client) => {
   var ip = client.remoteAddress;
-  var port = client.remotePort;
-  
+
   if (ip.substr(0,7) == "::ffff:") {
     ip = ip.substr(7);
   };
 
-  client.name = ip + ':' + port;
- 
-  console.log(client.name);
- 
+  client.name = ip + ':' + client.remotePort;
+  client.write('Hi ' + client.name + '\n');
+  console.log(client.name + ' just joined');
+  
   client.write('Connexion to chat server established!\n');
 
   clientList.push(client);
 
-   client.on('data', (data) => {
+  client.on('data', (data) => {
       broadcast(data, client);
   });
+
+  client.on('end', () => {
+    console.log(client.name + ' quit.');    
+    clientList.splice(clientList.indexOf(client), 1)
+  });
+
+  client.on('error', (e) => {
+    console.log(e);  
+  });
+
 });
 
+
+
 function broadcast(message, client) {
+  var cleanup = [];
   for (let i = 0; i < clientList.length; i++) {
     if (client !== clientList[i]) {
-      clientList[i].write(client.name + " says > " + message)
+      if (clientList[i].writable) {
+        clientList[i].write(client.name + " says > " + message)
+      } else {
+        cleanup.push(clientList[i]);
+        clientList[i].destroy();
+      }
     };
+  };
+  for (let i = 0; i < cleanup.length; i++) {
+    clientList.splice(clientList.indexOf(cleanup[i]),1);    
   };
 };
 
 chatServer.listen(9000);
+console.log('Chat Server started at 127.0.0.1:9000');
